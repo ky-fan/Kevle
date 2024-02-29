456c07ac4362be93a8b6aa4b2edc277adfaa9cd5
Kevin Fan <kyfan01@gmail.com>
2024-02-29 18:36:25 -0500

    finished form styling

--- react-vite/src/components/ConnectionComponents/ConnectionFormPage/ConnectionFormPage.jsx
+++ react-vite/src/components/ConnectionComponents/ConnectionFormPage/ConnectionFormPage.jsx
@@ -18,14 +18,11 @@ export function ConnectionFormPage() {
     const [answers3, setAnswers3] = useState("")
     const [answers4, setAnswers4] = useState("")
 
-    // const [isUpdate, setIsUpdate] = useState(false)
-    // const [hasSubmitted, setHasSubmitted] = useState(false)
     const [valErrors, setValErrors] = useState({})
     const currentUser = useSelector(state => state.session['user'])
 
     useEffect(() => {
         if (connectionId) {
-            // setIsUpdate(true)
             dispatch(thunkFetchConnectionById(connectionId)).then((oldConnection) => {
                 if (!(currentUser.id === oldConnection.userId)) navigate('/connections')
                 setTitle(oldConnection.title)
@@ -50,16 +47,15 @@ export function ConnectionFormPage() {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        // setHasSubmitted(false)
 
         const errObj = {}
         if (title.length >= 50) errObj.title = "Title must be less than 50 characters"
-        if (category1.length >= 50) errObj.category1 = "First category must be less than 30 characters"
-        if (category2.length >= 50) errObj.category2 = "Second category must be less than 30 characters"
-        if (category3.length >= 50) errObj.category3 = "Third category must be less than 30 characters"
-        if (category4.length >= 50) errObj.category4 = "Fourth category must be less than 30 characters"
+        if (category1.length >= 30) errObj.category1 = "First category must be less than 30 characters"
+        if (category2.length >= 30) errObj.category2 = "Second category must be less than 30 characters"
+        if (category3.length >= 30) errObj.category3 = "Third category must be less than 30 characters"
+        if (category4.length >= 30) errObj.category4 = "Fourth category must be less than 30 characters"
 
-        const answer1Arr = answers1.split(',')
+        const answer1Arr = answers1.split(',') //"1,2,3,4" => [1,2,3,4]
         if (answer1Arr.length !== 4) errObj.answer1Length = "The required 4 answers must be separated by commas"
         for (let answer of answer1Arr) {
             if (answer.length >= 16) errObj.answer1 = "All answers must be less than 16 characters"
@@ -85,11 +81,11 @@ export function ConnectionFormPage() {
 
         const allAnswersArr = answer1Arr.concat(answer2Arr, answer3Arr, answer4Arr)
         if (new Set(allAnswersArr).size !== allAnswersArr.length) errObj.uniqueAnswers = "All answers must be unique"
-        console.log(new Set(allAnswersArr), allAnswersArr)
+        // console.log(new Set(allAnswersArr), allAnswersArr)
 
         const allCategoriesArr = [category1, category2, category3, category4]
         if (new Set(allCategoriesArr).size !== allCategoriesArr.length) errObj.uniqueCategories = "All categories must be unique"
-        console.log(new Set(allCategoriesArr), allCategoriesArr)
+        // console.log(new Set(allCategoriesArr), allCategoriesArr)
 
         if (Object.values(errObj).length) {
             setValErrors(errObj)
@@ -145,59 +141,87 @@ export function ConnectionFormPage() {
                     <div className='connections-form-title-input-container'>
                         <div className='connections-form-input-header'>
                             <label htmlFor="title" className='connections-form-title-input'>Title</label>
-                            <input type="text" id='title' name='title' value={title} onChange={(e) => setTitle(e.target.value)} required placeholder='Title' />
+                            <input type="text" id='title' name='title' value={title} onChange={(e) => setTitle(e.target.value)} placeholder='Title' required />
                         </div>
 
-                        <div className='connections-form-title-error-container'>
+                        <div className='connections-form-error-container'>
                             {valErrors.uniqueCategories && <p>{valErrors.uniqueCategories}</p>}
                             {valErrors.uniqueAnswers && <p>{valErrors.uniqueAnswers}</p>}
                             {valErrors.title && <p>{valErrors.title}</p>}
                         </div>
                     </div>
 
-                    <div>
-                        {valErrors.category1 && <p>{valErrors.category1}</p>}
-                        <label htmlFor="category1">Category Description</label>
-                        <input type="text" id='category1' name='first category' value={category1} onChange={(e) => setCategory1(e.target.value)} required />
+                    <div className='connections-form-category-container'>
+                        <div className='connections-form-category-border'>
+                            <div className='connections-form-category-input-header' id='connections-form-green-category'>
+                                <label htmlFor="category1" className='connections-form-category-label'>Category Description</label>
+                                <input type="text" id='category1' name='first category' value={category1} onChange={(e) => setCategory1(e.target.value)} className='connections-form-first-input' required />
 
-                        {valErrors.answer1Length && <p>{valErrors.answer1Length}</p>}
-                        {valErrors.answer1 && <p>{valErrors.answer1}</p>}
-                        <label htmlFor="answers1">Answers (Comma-separated)</label>
-                        <input type="text" id='answers1' name='first category answers' value={answers1} onChange={(e) => setAnswers1(e.target.value)} required />
+                                <label htmlFor="answers1" className='connections-form-category-label'>Answers (Comma-separated)</label>
+                                <input type="text" id='answers1' name='first category answers' value={answers1} onChange={(e) => setAnswers1(e.target.value)} required />
+                            </div>
+                        </div>
+
+                        <div className='connections-form-error-container'>
+                            {valErrors.category1 && <p>{valErrors.category1}</p>}
+                            {valErrors.answer1Length && <p>{valErrors.answer1Length}</p>}
+                            {valErrors.answer1 && <p>{valErrors.answer1}</p>}
+                        </div>
                     </div>
 
-                    <div>
-                        {valErrors.category2 && <p>{valErrors.category2}</p>}
-                        <label htmlFor="category2">Category Description</label>
-                        <input type="text" id='category2' name='second category' value={category2} onChange={(e) => setCategory2(e.target.value)} required />
+                    <div className='connections-form-category-container'>
+                        <div className='connections-form-category-border'>
+                            <div className='connections-form-category-input-header' id='connections-form-yellow-category'>
+                                <label htmlFor="category2" className='connections-form-category-label'>Category Description</label>
+                                <input type="text" id='category2' name='second category' value={category2} onChange={(e) => setCategory2(e.target.value)} className='connections-form-first-input' required />
 
-                        {valErrors.answer2Length && <p>{valErrors.answer2Length}</p>}
-                        {valErrors.answer2 && <p>{valErrors.answer2}</p>}
-                        <label htmlFor="answers2">Answers (Comma-separated)</label>
-                        <input type="text" id='answers2' name='second category answers' value={answers2} onChange={(e) => setAnswers2(e.target.value)} required />
+                                <label htmlFor="answers2" className='connections-form-category-label'>Answers (Comma-separated)</label>
+                                <input type="text" id='answers2' name='second category answers' value={answers2} onChange={(e) => setAnswers2(e.target.value)} required />
+                            </div>
+                        </div>
+                        <div className='connections-form-error-container'>
+                            {valErrors.category2 && <p>{valErrors.category2}</p>}
+                            {valErrors.answer2Length && <p>{valErrors.answer2Length}</p>}
+                            {valErrors.answer2 && <p>{valErrors.answer2}</p>}
+                        </div>
                     </div>
 
-                    <div>
-                        {valErrors.category3 && <p>{valErrors.category3}</p>}
-                        <label htmlFor="category3">Category Description</label>
-                        <input type="text" id='category3' name='third category' value={category3} onChange={(e) => setCategory3(e.target.value)} required />
+                    <div className='connections-form-category-container'>
+                        <div className='connections-form-category-border'>
+                            <div className='connections-form-category-input-header' id='connections-form-cyan-category'>
+                                <label htmlFor="category3" className='connections-form-category-label'>Category Description</label>
+                                <input type="text" id='category3' name='third category' value={category3} onChange={(e) => setCategory3(e.target.value)} className='connections-form-first-input' required />
 
-                        {valErrors.answer3Length && <p>{valErrors.answer3Length}</p>}
-                        {valErrors.answer3 && <p>{valErrors.answer3}</p>}
-                        <label htmlFor="answers3">Answers (Comma-separated)</label>
-                        <input type="text" id='answers3' name='third category answers' value={answers3} onChange={(e) => setAnswers3(e.target.value)} required />
+                                <label htmlFor="answers3" className='connections-form-category-label'>Answers (Comma-separated)</label>
+                                <input type="text" id='answers3' name='third category answers' value={answers3} onChange={(e) => setAnswers3(e.target.value)} required />
+                            </div>
+                        </div>
+                        <div className='connections-form-error-container'>
+                            {valErrors.category3 && <p>{valErrors.category3}</p>}
+                            {valErrors.answer3Length && <p>{valErrors.answer3Length}</p>}
+                            {valErrors.answer3 && <p>{valErrors.answer3}</p>}
+                        </div>
                     </div>
-                    <div>
-                        {valErrors.category4 && <p>{valErrors.category4}</p>}
-                        <label htmlFor="category4">Category Description</label>
-                        <input type="text" id='category4' name='fourth category' value={category4} onChange={(e) => setCategory4(e.target.value)} required />
+                    <div className='connections-form-category-container'>
+                        <div className='connections-form-category-border'>
+                            <div className='connections-form-category-input-header' id='connections-form-pink-category'>
+                                <label htmlFor="category4" className='connections-form-category-label'>Category Description</label>
+                                <input type="text" id='category4' name='fourth category' value={category4} onChange={(e) => setCategory4(e.target.value)} className='connections-form-first-input' required />
 
-                        {valErrors.answer4Length && <p>{valErrors.answer4Length}</p>}
-                        {valErrors.answer4 && <p>{valErrors.answer4}</p>}
-                        <label htmlFor="answers4">Answers (Comma-separated)</label>
-                        <input type="text" id='answers4' name='fourth category answers' value={answers4} onChange={(e) => setAnswers4(e.target.value)} required />
+                                <label htmlFor="answers4" className='connections-form-category-label'>Answers (Comma-separated)</label>
+                                <input type="text" id='answers4' name='fourth category answers' value={answers4} onChange={(e) => setAnswers4(e.target.value)} required />
+                            </div>
+                        </div>
+
+                        <div className='connections-form-error-container'>
+                            {valErrors.category4 && <p>{valErrors.category4}</p>}
+                            {valErrors.answer4Length && <p>{valErrors.answer4Length}</p>}
+                            {valErrors.answer4 && <p>{valErrors.answer4}</p>}
+                        </div>
                     </div>
-                    <button type='submit'>Generate</button>
+                    <div className='connections-form-button-container'>
+                        <button type='submit' className='connections-form-button'>Generate</button>
+                    </div>
                 </form>
             </div>
         </div>
