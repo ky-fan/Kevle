f986ecf57f9c0ffa6fde8daa7f37015c079a40ab
Kevin Fan <kyfan01@gmail.com>
2024-03-04 11:23:46 -0500

    refactored shuffle

--- react-vite/src/components/ConnectionComponents/ConnectionBoard/ConnectionBoard.jsx
+++ react-vite/src/components/ConnectionComponents/ConnectionBoard/ConnectionBoard.jsx
@@ -18,28 +18,40 @@ export function ConnectionBoard({ connectionId }) {
     // Contains the randomized answers
     const [shuffledArr, setShuffledArr] = useState([])
 
-    // function shuffle(arr) {
-    //     for (let i = arr.length - 1; i < 0; i--) {
-    //         let j = Math.floor(Math.random() * (i + 1))
-    //         let temp = arr[i]
-    //         arr[i] = arr[j]
-    //         arr[j] = temp
-    //     }
-    //     return arr
-    // }
+    function shuffle(arr) {
+
+        const returnArr = arr.slice(0)
+        for (let i = returnArr.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1))
+            let temp = returnArr[i]
+            returnArr[i] = returnArr[j]
+            returnArr[j] = temp
+        }
+        return returnArr
+    }
 
     const answerArr = connection?.answers
 
+    useEffect(() => {
+        setShuffledArr(connection?.answers)
+        if (connection) setShuffledArr(shuffle(connection.answers))
+    }, [connection])
+
+
     console.log("this is shuffled ", shuffledArr)
 
-    // Stores [categoryNumber, category, answer1, answer2, answer3, answer4] to pass into ConnectionAnswerBar
-    const categoryObj = {}
-    categoryObj.category1 = [1, connection?.categories[0]].concat(answerArr?.slice(0, 4))
-    categoryObj.category2 = [2, connection?.categories[1]].concat(answerArr?.slice(4, 8))
-    categoryObj.category3 = [3, connection?.categories[2]].concat(answerArr?.slice(8, 12))
-    categoryObj.category4 = [4, connection?.categories[3]].concat(answerArr?.slice(12, 16))
+    const connectionArr = connection?.categories
 
-    const shuffledFirstRow = answerArr?.slice(0, 4)
+    if (connection) {
+        const categoryObj = {}
+        categoryObj.category1 = [1, connectionArr[0]].concat(answerArr?.slice(0, 4))
+        categoryObj.category2 = [2, connectionArr[1]].concat(answerArr?.slice(4, 8))
+        categoryObj.category3 = [3, connectionArr[2]].concat(answerArr?.slice(8, 12))
+        categoryObj.category4 = [4, connectionArr[3]].concat(answerArr?.slice(12, 16))
+
+    }
+    const shuffledFirstRow = shuffledArr?.slice(0, 4)
+    // Stores [categoryNumber, category, answer1, answer2, answer3, answer4] to pass into ConnectionAnswerBar
 
     // const randomAnswers = shuffle(answerArr)
 
@@ -52,12 +64,15 @@ export function ConnectionBoard({ connectionId }) {
             </div>
             <div className='connection-board-row'>
                 {gameState[1] > 0 && <ConnectionAnswerBar category={categoryObj[`category${gameState[1]}`]} />}
+                {gameState[0] === 0 && shuffledArr?.slice(4, 8).map(word => (<ConnectionWordTile key={word} word={word} />))}
             </div>
             <div className='connection-board-row'>
                 {gameState[2] > 0 && <ConnectionAnswerBar category={categoryObj[`category${gameState[2]}`]} />}
+                {gameState[0] === 0 && shuffledArr?.slice(8, 12).map(word => (<ConnectionWordTile key={word} word={word} />))}
             </div>
             <div className='connection-board-row'>
                 {gameState[3] > 0 && <ConnectionAnswerBar category={categoryObj[`category${gameState[3]}`]} />}
+                {gameState[0] === 0 && shuffledArr?.slice(12, 16).map(word => (<ConnectionWordTile key={word} word={word} />))}
             </div>
             {/* {answerArr?.map(word => (
                 <ConnectionWordTile key={word} word={word} />
