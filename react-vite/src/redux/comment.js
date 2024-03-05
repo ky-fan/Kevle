f028b87a3dca01df2e7901e2292a14e28fb47a48
Kevin Fan <kyfan01@gmail.com>
2024-03-05 11:39:46 -0500

    more comment thunks

--- react-vite/src/redux/comment.js
+++ react-vite/src/redux/comment.js
@@ -8,6 +8,11 @@ export const loadComments = comments => ({
     payload: comments
 })
 
+export const loadCommentById = comment => ({
+    type: LOAD_COMMENTS,
+    comment
+})
+
 // Thunk action creators
 export const thunkFetchComments = () => async dispatch => {
     const res = await fetch('/api/comments')
@@ -18,11 +23,26 @@ export const thunkFetchComments = () => async dispatch => {
     } else return { 'message': 'fetch comments thunk error' }
 }
 
+export const thunkFetchCommentById = commentId => async dispatch => {
+    const res = await fetch(`/api/comments/${commentId}`)
+    if (res.ok) {
+        const comment = await res.json()
+        dispatch(loadCommentById(comment))
+        return comment
+    } else return { 'message': 'fetch comment by id thunk error' }
+}
+
 const commentReducer = (state = {}, action) => {
     switch (action.type) {
         case LOAD_COMMENTS: {
             const newCommentState = { ...state }
             action.playload.comments.forEach(comment => { newCommentState[comment.id] = comment })
+            return newCommentState
+        }
+
+        case LOAD_COMMENT_BY_ID: {
+            const newCommentState = { ...state }
+            newCommentState[action.comment.id] = action.comment
             return newCommentState
         }
 
