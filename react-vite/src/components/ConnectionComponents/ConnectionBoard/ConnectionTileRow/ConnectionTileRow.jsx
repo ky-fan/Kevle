3c8ca63f84c4503e512957c63ca1faf76567253d
Kevin Fan <kyfan01@gmail.com>
2024-03-06 17:29:01 -0500

    creating tile rows

---
+++ react-vite/src/components/ConnectionComponents/ConnectionBoard/ConnectionTileRow/ConnectionTileRow.jsx
@@ -0,0 +1,13 @@
+import './ConnectionTileRow.css'
+import ConnectionWordTile from '../ConnectionWordTile/ConnectionWordTile'
+
+export function ConnectionTileRow(words, setGuessArr, guessArr) {
+
+    return (
+        <div>
+            {/* {displayArr?.splice(0, 4)?.map(word => (<ConnectionWordTile key={word} word={word} setGuessArr={setGuessArr} guessArr={guessArr} />))} */}
+        </div>
+    )
+}
+
+export default ConnectionTileRow
