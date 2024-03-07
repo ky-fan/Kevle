cf8c46ad5780d9885398efdc6f01341a70443a8e
Kevin Fan <kyfan01@gmail.com>
2024-03-07 15:27:40 -0500

    features styled

--- react-vite/src/components/ConnectionComponents/ConnectionBoard/ConnectionBoard.jsx
+++ react-vite/src/components/ConnectionComponents/ConnectionBoard/ConnectionBoard.jsx
@@ -110,7 +110,7 @@ export function ConnectionBoard({ connection }) {
 
     const submitGuess = e => {
         e.preventDefault()
-
+        if (!guessArr.length) return
         // Stores sets containing each group of answers for comparison to guesses
         const answerSetObj = {}
         for (let i = 1; i < 5; i++) {
@@ -167,7 +167,7 @@ export function ConnectionBoard({ connection }) {
 
             <div className='connection-board-button-container'>
                 <button onClick={() => setShuffledArr(shuffle(connection.answers))} className={`connection-board-button click-swing`} id={gameStatus === 'playing' ? "" : "disabled-connection-button"}>Shuffle</button>
-                <button onClick={() => setGuessArr([])} className={`connection-board-button`} id={guessArr.length > 0 ? "" : "disabled-connection-button"}>Deselect All</button>
+                <button onClick={() => setGuessArr([])} className={`connection-board-button`} id={guessArr.length > 0 ? "" : "disabled-connection-button"}>Deselect All - bugged</button>
                 <button onClick={submitGuess} className={`connection-board-button`} id={guessArr.length === 4 ? "" : "disabled-connection-button"}>Submit</button>
             </div>
 
