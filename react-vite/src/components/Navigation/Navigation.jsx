8cbe345e03b978e69e0373086403001445f96252
Kevin Fan <kyfan01@gmail.com>
2024-03-11 00:21:04 -0400

    styling

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -2,21 +2,28 @@ import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 import { useSelector } from "react-redux";
+import { TbChartGridDotsFilled } from "react-icons/tb";
+
+
 
 function Navigation() {
   const user = useSelector(state => state.session.user)
 
   return (
     <div className="navbar">
-      <div className="nav-logo-div">
-        <NavLink to='/' className='nav-logo' title="Splash Page">
-          <p>Logo Here</p>
-        </NavLink>
-      </div>
+      <NavLink to='/' className='nav-logo' title="Splash Page">
+        <div className="nav-logo-div">
+          <TbChartGridDotsFilled className="nav-logo-icon" />
+          <div className="nav-logo-title-container">
+            <span className="nav-logo-title">Kevle</span>
+            <span className="nav-logo-subtitle">by Kevin Fan</span>
+          </div>
+        </div>
+      </NavLink>
       <div className="nav-buttons">
         <div className="nav-buttons-icons-div">
-          <NavLink to='/connections'>All Connections</NavLink>
-          {user && <NavLink to='/connections/new'>Create Connections Game</NavLink>}
+          <NavLink to='/connections' className='navigation-link'>All Connections</NavLink>
+          {user && <NavLink to='/connections/new' className='navigation-link'>Create Connections Game</NavLink>}
         </div>
         <ProfileButton className="profile-button" title="User-Options" />
       </div>
--- react-vite/src/components/SplashPage/SplashPage.jsx
+++ react-vite/src/components/SplashPage/SplashPage.jsx
@@ -1,10 +1,26 @@
 import './SplashPage.css'
 
+import { FaGithubSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 export function SplashPage() {
     return (
-        <div>
-            <h1>Welcome!</h1>
-        </div>
+        <div className='splash-page-container'>
+            <div className='splash-page-body'>
+                <div className='splash-welcome-div'>
+                    <h1>Welcome to Kevle!</h1>
+                    <h3>A site where you can create and play your own NYT Connections and more! Created using React/Redux and Flask.</h3>
+                </div>
+                <div>
+                    <h1>Meet the Developer:</h1>
+                    <div className='splash-dev-name-icon-div'>
+                        <h2>Kevin Fan</h2>
+                        <a href="https://github.com/Kyfan01"><FaGithubSquare className='splash-icon' title='Github' /></a>
+                        {/* <a href="https://www.linkedin.com/in/kevin-fan-20475a162/"><FaLinkedin className='splash-icon' /></a> */}
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 
